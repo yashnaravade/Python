f44bc3b949761d7885176c542712efc8d6b26fc8
Yash Naravade <yashdip123@gmail.com>
2023-02-28 15:51:31 +0530

    Tuple

--- tuple1.py
+++ tuple1.py
@@ -2,3 +2,16 @@ myTuple = ("yash", 23, True, 4.5)
 print(myTuple)
 print(type(myTuple))
 print(myTuple[0])
+print(myTuple.count('yash'))
+
+for i in myTuple:
+    print(i)
+
+# myTuple[1] = 24 # TypeError: 'tuple' object does not support item assignment
+
+myTuple.append(5) # AttributeError: 'tuple' object has no attribute 'append'
+
+myTuple = ("yash", 23, True, 4.5, 5)
+print(myTuple)  # ('yash', 23, True, 4.5, 5)
+
+
