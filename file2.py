71eee904af1f74ff94d960b000b467cc8d67d2ed
Yash Naravade <yashdip123@gmail.com>
2023-03-05 23:36:58 +0530

    file handling edit

--- file2.py
+++ file2.py
@@ -1,6 +1,16 @@
-# delete a file
-import os
-if os.path.exists("file1.txt"):
-  os.remove("file1.txt")
-else:
-    print("The file does not exist")
+# # delete a file
+# import os
+# if os.path.exists("file1.txt"):
+#   os.remove("file1.txt")
+# else:
+#     print("The file does not exist")
+
+# edit a file 
+f = open("file2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+
+# open and read the file after the appending:
+f = open("file2.txt", "r")
+print(f.read())
+
