98d31224c40081be6a09cff82664f6e14fca6353
Yash Naravade <yashdip123@gmail.com>
2023-03-31 16:04:59 +0530

    define a class

---
+++ python_class.py
@@ -0,0 +1,13 @@
+class MyClass:
+    print("Hey! this is my first python class!")
+
+    def hello(self):
+        print("Hello World!")
+    
+    a= 69;
+    b= 420;
+
+
+Myc = MyClass()
+Myc.hello()
+print(Myc.a, Myc.b)
