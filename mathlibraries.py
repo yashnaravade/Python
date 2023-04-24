f71a0100f1d71ee9e551d2097a1e802a783d81c9
Yash Naravade <yashdip123@gmail.com>
2023-04-24 13:46:23 +0530

    math functions

---
+++ mathlibraries.py
@@ -0,0 +1,9 @@
+import math
+
+print("The value of pi is" , math.pi)
+
+print("Square root of 16 is" , math.sqrt(16))
+
+print("Cosine of 10 is" , math.cos(0))
+
+print("Factorial of 5 is" , math.factorial(5))
